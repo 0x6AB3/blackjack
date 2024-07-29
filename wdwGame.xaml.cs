a407933676447b1b92d828ee93c55eb9d2a4fa10
Patryk Gluszkiewicz <gluszkiewiczp@cardiff.ac.uk>
2024-07-29 23:18:51 +0100

    Created initial game UI

--- wdwGame.xaml.cs
+++ wdwGame.xaml.cs
@@ -14,9 +14,9 @@ namespace blackjack
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class wdwGame : Window
     {
-        public MainWindow()
+        public wdwGame()
         {
             InitializeComponent();
         }
